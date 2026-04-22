fa9ff95e2bd1000062c2da6940c118a4e059d181
GuiParchs <guilherme.rpersch036@gmail.com>
2026-04-22 11:02:19 -0300

    add scoreboard and center line

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local push = require 'lib.push'
 local Ball = require 'src.Ball'
 local Paddle = require 'src.Paddle'
 
+local gameState = require 'src.GameState'
+
 VIRTUAL_WIDTH = 320
 VIRTUAL_HEIGHT = 240
 
@@ -78,19 +80,64 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+local function displayScore()
+    love.graphics.setFont(largeFont)
+
+    local halfWidth = VIRTUAL_WIDTH / 2
+    local h = VIRTUAL_HEIGHT / 5
+
+    -- p1
+    love.graphics.printf(
+        tostring(gameState.score[1]),
+        0,
+        h,
+        halfWidth,
+        'center'
+    )
+
+    -- p2
+    love.graphics.printf(
+        tostring(gameState.score[2]),
+        halfWidth,
+        h,
+        halfWidth,
+        'center'
+    )
+end
+
+local function drawCenterLine(segmentHeight, gap)
+    local total = segmentHeight + gap
+    local x = math.floor(VIRTUAL_WIDTH / 2 - 1)
+
+    local y = gap
+
+    while y + segmentHeight <= VIRTUAL_HEIGHT - gap do
+        love.graphics.rectangle('fill', x, y, 2, segmentHeight)
+        y = y + total
+    end
+end
+
 function love.draw()
     push:start()
 
     love.graphics.clear(BG_COLOR[1], BG_COLOR[2], BG_COLOR[3], BG_COLOR[4])
 
+    love.graphics.setColor(0.5, 0.5, 0.5, 1)
+    drawCenterLine(12, 6)
+    love.graphics.setColor(1, 1, 1, 1)
+
     love.graphics.setFont(smallFont)
-    love.graphics.printf('Welcome to pong!', 0, 10, VIRTUAL_WIDTH, 'center')
-    love.graphics.printf('Press select to start', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Welcome to Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter to start', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
 
     paddle1:render()
     paddle2:render()
 
     ball:render()
+
+    displayScore()
 
     push:finish()
 end
---
+++ src/GameState.lua
@@ -0,0 +1,7 @@
+local GameState = {}
+
+GameState.score = {0, 0}
+
+GameState.state = 'start'
+
+return GameState
