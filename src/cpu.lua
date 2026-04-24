2fb61bf9099fb6107cc1853effbbe9bfec7ed502
GuiParchs <guilherme.rpersch036@gmail.com>
2026-04-23 22:39:02 -0300

    add taunting to cpu

--- src/cpu.lua
+++ src/cpu.lua
@@ -6,6 +6,9 @@ local MIN_SERVE_DELAY = 0.75
 local MAX_SERVE_DELAY = 3.75
 local SERVE_DELAY_SHITF = 0.8 -- decreases serve delay the more points P1 has
 
+local TAUNT_SPEED = 20
+local TAUNT_RANGE = 15
+
 local MIN_REACTION_TIME = 0.01
 local MAX_REACTION_TIME = 0.17
 
@@ -27,6 +30,10 @@ local function getRandomNumber(min, max, serveRelated)
     if serveRelated then
         local shift = gameState.score[1] * SERVE_DELAY_SHITF
         max = max - shift
+
+        if min < 0 then
+            shift = -shift
+        end
         min = min - shift / 5
     end
 
@@ -53,6 +60,7 @@ function cpu.handleGame(dt)
         lastGameState = gameState.state
         paddle:move(0)
         currentTask = nil
+        targetY = nil
     end
 
     -- Update timers
@@ -61,12 +69,11 @@ function cpu.handleGame(dt)
 
     -- Serve state
     if gameState.state == 'serve' then
-
         if not currentTask then
             -- Start serve countdown
             if gameState.servingPlayer == 2 then
                 currentTask = 'serve'
-                taskTimer = getRandomNumber(MIN_SERVE_DELAY, MAX_SERVE_DELAY)
+                taskTimer = getRandomNumber(MIN_SERVE_DELAY, MAX_SERVE_DELAY, true)
             else
                 currentTask = 'waiting_serve' -- or wait for player to serve
             end
@@ -76,6 +83,12 @@ function cpu.handleGame(dt)
             ball:serve(2)
             gameState.state = 'playing'
             currentTask = nil -- reset task
+        end
+
+        -- TAUNT
+        if currentTask == 'waiting_serve' or currentTask == 'serve' then
+            local oscillation = math.sin(love.timer.getTime() * TAUNT_SPEED) * TAUNT_RANGE
+            targetY = VIRTUAL_HEIGHT / 2 + oscillation + getRandomNumber(-MAX_TARGET_OFFSET, MAX_TARGET_OFFSET, true)
         end
 
     -- Playing state
@@ -89,15 +102,15 @@ function cpu.handleGame(dt)
             targetY = ball.y + (ball.dy * LEAD_TIME) + ball.size / 2 + getRandomNumber(-MAX_TARGET_OFFSET, MAX_TARGET_OFFSET)
             taskTimer = getRandomNumber(MIN_REACTION_TIME, MAX_REACTION_TIME)
         end
+    end
 
-        -- Move paddle
-        if targetY then
-            if moveTimer > 0 then return end
+    -- Move paddle
+    if targetY then
+        if moveTimer > 0 then return end
 
-            cpu.move()
+        cpu.move()
 
-            moveTimer = getRandomNumber(MIN_REACTION_TIME, MAX_REACTION_TIME)
-        end
+        moveTimer = getRandomNumber(MIN_REACTION_TIME, MAX_REACTION_TIME)
     end
 end
 
